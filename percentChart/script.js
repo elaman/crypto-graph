009225d7e5377149b0787488324350bb96c21974
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-11-23 00:03:11 +0600

    Input for add more coins

--- percentChart/script.js
+++ percentChart/script.js
@@ -27,17 +27,24 @@ var chart = LightweightCharts.createChart(document.body, {
 	},
 });
 
-var areaSeries = chart.addAreaSeries({
+var btcusdt = chart.addAreaSeries({
 	topColor: 'rgba(67, 83, 254, 0.7)',
 	bottomColor: 'rgba(67, 83, 254, 0.3)',
 	lineColor: 'rgba(67, 83, 254, 1)',
 	lineWidth: 2,
 });
 
-var extraSeries = chart.addAreaSeries({
+var ethusdt = chart.addAreaSeries({
 	topColor: 'rgba(255, 192, 0, 0.7)',
 	bottomColor: 'rgba(255, 192, 0, 0.3)',
 	lineColor: 'rgba(255, 192, 0, 1)',
+	lineWidth: 2,
+});
+
+var solusdt = chart.addAreaSeries({
+	topColor: 'rgba(907, 173, 84, 0.7)',
+	bottomColor: 'rgba(707, 883, 354, 0.3)',
+	lineColor: 'rgba(502, 603, 554, 1)',
 	lineWidth: 2,
 });
 
@@ -56,16 +63,15 @@ fetch(`http://127.0.0.1:9665/fetchAPI?endpoint=https://api.binance.com/api/v3/kl
 		for (let i = 0; i < cdata.length; i++) {
 			cdata[i].value = cdata[i].value / (maxPrice / 100);
 		}
-		areaSeries.setData(cdata);
+		btcusdt.setData(cdata);
 	})
-	.catch(err => log(err))
 
 //Dynamic Chart
-const socket = io.connect('http://127.0.0.1:5500/');
+let socket = io.connect('http://127.0.0.1:5500/');
 
 socket.on('KLINE', (pl) => {
 	//log(pl);
-	areaSeries.update(pl);
+	btcusdt.update(pl);
 });
 
 fetch(`http://127.0.0.1:9665/fetchAPI?endpoint=https://api.binance.com/api/v3/klines?symbol=ETHUSDT&interval=1d&limit=1000`)
@@ -83,18 +89,56 @@ fetch(`http://127.0.0.1:9665/fetchAPI?endpoint=https://api.binance.com/api/v3/kl
 		for (let i = 0; i < cdata.length; i++) {
 			cdata[i].value = cdata[i].value / (maxPrice / 100);
 		}
-		extraSeries.setData(cdata);
+		ethusdt.setData(cdata);
 	})
-	.catch(err => log(err))
 
 //Dynamic Chart
-const socket1 = io.connect('http://127.0.0.1:5500/');
+socket = io.connect('http://127.0.0.1:5500/');
 
-socket1.on('KLINE', (pl) => {
+socket.on('KLINE', (pl) => {
 	//log(pl);
-	extraSeries.update(pl);
+	ethusdt.update(pl);
 });
 
-let input = document.createElement("input");
 
-document.write
+let input = document.getElementById("input");
+let btn = document.getElementById("btn");
+
+btn.addEventListener('click', function addCharts(event) {
+	let url = `http://127.0.0.1:9665/fetchAPI?endpoint=https://api.binance.com/api/v3/klines?symbol=${input.value}&interval=1d&limit=1000`;
+
+	console.log(url)
+
+	function getAPI() {
+		fetch(url)
+			.then(res => res.json())
+			.then(data => {
+				let maxPrice = 0;
+				const cdata = data.map(d => {
+					let price = parseFloat(d[2]);
+					if (maxPrice < price) {
+						maxPrice = price;
+					}
+					return { time: d[0] / 1000, value: price }
+				});
+
+				console.log(cdata)
+
+				for (let i = 0; i < cdata.length; i++) {
+					cdata[i].value = cdata[i].value / (maxPrice / 100);
+				}
+				solusdt.setData(cdata);
+			})
+
+		//Dynamic Chart
+		socket = io.connect('http://127.0.0.1:5500/');
+
+		socket.on('KLINE', (pl) => {
+			//log(pl);
+			solusdt.update(pl);
+		});
+	}
+	btn.addEventListener("click", function getValue(event){
+		getAPI();
+	})
+});
