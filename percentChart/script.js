fcc4378aae6db1b4f3e51dc4afddc6e844f27cd3
Tatyna Kubatova <tatynakubatova74@gmail.com>
2021-11-23 15:03:04 +0600

    Add comments

--- percentChart/script.js
+++ percentChart/script.js
@@ -1,3 +1,40 @@
+function fetchCoin(pair) {
+	if( !pair ){
+		return
+	}
+	//fetch data from BINANCE
+	let url = `https://api.binance.com/api/v3/klines?symbol=${pair}&interval=1d&limit=1000`;
+	fetch(url)
+		.then(res => res.json())
+		.then(data => {
+			//process data from BINANCE
+			let maxPrice = 0;
+			const cdata = data.map(d => {
+				//determined max price of coin
+				let price = parseFloat(d[2]);
+				if (maxPrice < price) {
+					maxPrice = price;
+				}
+				return { time: d[0] / 1000, value: price }
+			});
+
+			for (let i = 0; i < cdata.length; i++) {
+				cdata[i].value = cdata[i].value / (maxPrice / 100);
+			}
+			//create style for chartline
+			let r = Math.round(Math.random() * 255);
+			let g = Math.round(Math.random() * 255);
+			let b = Math.round(Math.random() * 255);
+			var chartColor = chart.addAreaSeries({
+				topColor: `rgba(${r}, ${g}, ${b}, .7)`,
+				bottomColor: `rgba(${r}, ${g}, ${b}, .3)`,
+				lineColor: `rgba(${r}, ${g}, ${b}, 1)`,
+				lineWidth: 2,
+			});
+			chartColor.setData(cdata);
+		})
+}
+
 //Create a chart
 var chart = LightweightCharts.createChart(document.body, {
 	width: 1000,
@@ -32,46 +69,17 @@ var chart = LightweightCharts.createChart(document.body, {
 let input = document.getElementById("input");
 let btn = document.getElementById("btn");
 
-//
-let preferCoin = localStorage.getItem("preferCoin") ?? [];
+//get data from localStorage
+let preferCoin = (localStorage.getItem("preferCoin") ?? "").split(",");
 
 //add EventListener on click
 btn.addEventListener('click', function add(event) {
-
+	fetchCoin(input.value)
 	preferCoin.push(input.value);
-
-	//fetch data from BINANCE
-	let url = `https://api.binance.com/api/v3/klines?symbol=${input.value}&interval=1d&limit=1000`;
-	fetch(url)
-		.then(res => res.json())
-		.then(data => {
-			//proces data from BINANCE
-			let maxPrice = 0;
-			const cdata = data.map(d => {
-				//determined max price of coin
-				let price = parseFloat(d[2]);
-				if (maxPrice < price) {
-					maxPrice = price;
-				}
-				return { time: d[0] / 1000, value: price }
-			});
-
-			for (let i = 0; i < cdata.length; i++) {
-				cdata[i].value = cdata[i].value / (maxPrice / 100);
-			}
-			//create style for chartline
-			let r = Math.round(Math.random() * 255);
-			let g = Math.round(Math.random() * 255);
-			let b = Math.round(Math.random() * 255);
-			var chartColor = chart.addAreaSeries({
-				topColor: `rgba(${r}, ${g}, ${b}, .7)`,
-				bottomColor: `rgba(${r}, ${g}, ${b}, .3)`,
-				lineColor: `rgba(${r}, ${g}, ${b}, 1)`,
-				lineWidth: 2,
-			});
-
-			chartColor.setData(cdata);
-
-			localStorage.setItem("preferCoin", preferCoin);
-		})
+	localStorage.setItem("preferCoin", preferCoin);
 });
+
+//get saved data on page refresh
+document.body.onload = () => {
+	preferCoin.forEach(element => fetchCoin(element));
+}
