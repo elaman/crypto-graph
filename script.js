e0d6d6debeb69fc5af39da6af9a5d840fc08a9c7
Elaman <elaman@ima.sh>
2022-02-22 20:23:47 -0500

    Use websockets

--- script.js
+++ script.js
@@ -41,9 +41,7 @@ try {
 } catch (error) {}
 
 // Add existing coins to the chart.
-Object.keys(coinPairs).forEach((coinPair) =>
-  addCoinPair(coinPair)
-);
+Object.keys(coinPairs).forEach((coinPair) => addCoinPair(coinPair));
 
 // Prepare new coin form.
 const coinPairInput = document.querySelector("input");
@@ -67,20 +65,41 @@ function addCoinPair(coinPair) {
     .then((response) => response.json())
     .then((data) => {
       // Add line to the chart.
-      const color = coinPairs.hasOwnProperty(coinPair) ? coinPairs[coinPair].color : generateColor();
+      const color = coinPairs.hasOwnProperty(coinPair)
+        ? coinPairs[coinPair].color
+        : generateColor();
       const coinPairLine = chart.addLineSeries({ title: coinPair, color });
-      coinPairLine.setData(processExchangeData(data));
+      const [lineData, maxPrice] = processExchangeData(data);
+      coinPairLine.setData(lineData);
 
       // Add DOM elements responsible for coin pair.
       addCoinPairDOM(coinPairLine);
 
       // Update local storage.
-      coinPairs[coinPair] = { color };
+      coinPairs[coinPair] = { color, maxPrice };
       localStorage.setItem("coinPairs", JSON.stringify(coinPairs));
+
+      // Websocket
+      const sock = new WebSocket(
+        `wss://stream.binance.com:9443/ws/${coinPair.toLocaleLowerCase()}@kline_1d`
+      );
+      sock.addEventListener("message", function (event) {
+        const data = JSON.parse(event.data);
+        coinPairs[coinPair].maxPrice =
+          coinPairs[coinPair].maxPrice < data.k.c
+            ? data.k.c
+            : coinPairs[coinPair].maxPrice;
+        coinPairLine.update({
+          time: data.k.t / 1000,
+          value: data.k.c / (coinPairs[coinPair].maxPrice / 100),
+        });
+      });
     })
     .catch((error) => {
       // Process errors.
-      alert(`Error adding the ${coinPair} coin pair. Check if format is valid.`);
+      alert(
+        `Error adding the ${coinPair} coin pair. Check if format is valid.`
+      );
       console.error(error);
     });
 }
@@ -115,20 +134,23 @@ function addCoinPairDOM(coinPairLine) {
 }
 
 function processExchangeData(data, maxPrice = 0) {
-  return data
-    .map(([time, , price]) => {
-      // Determine max price of the pair throughout the history.
-      price = parseFloat(price);
-      maxPrice = maxPrice < price ? price : maxPrice;
-      // Return relevant data.
-      return { time, price };
-    })
-    .map(({ time, price }) => ({
-      // We need seconds.
-      time: time / 1000,
-      // Calculate value based on max price.
-      value: price / (maxPrice / 100),
-    }));
+  return [
+    data
+      .map(([time, , price]) => {
+        // Determine max price of the pair throughout the history.
+        price = parseFloat(price);
+        maxPrice = maxPrice < price ? price : maxPrice;
+        // Return relevant data.
+        return { time, price };
+      })
+      .map(({ time, price }) => ({
+        // We need seconds.
+        time: time / 1000,
+        // Calculate value based on max price.
+        value: price / (maxPrice / 100),
+      })),
+    maxPrice,
+  ];
 }
 
 const colorElement = document.createElement("div");
